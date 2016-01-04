4125aa987198e824c0d0691abfe36423a5d38b22
Aleksander Herforth Rendtslev <aleksanderhr@live.com>
2016-01-04 13:58:39 +0100

    updated scrpt

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
 var shell = require('shelljs');
+var _ = require('lodash');
 
 
 function Plugin(options) {
-  if(!options.type) {throw Error("No run type defined!");}
-  if(!options.path) {throw Error("No path defined!");}
   var self = this;
-
-  var defaultOptions = {
-    type: "shell",
-    path: "/"
-  };
-
-  self.options = _.extend({}, defaultOptions, options);
-
 }
 
 Plugin.prototype.apply = function (compiler) {
   var self = this;
 
-  compiler.plugin('compile', function (params) {
-    if(this.options.type === "shell"){
-      var result = shell.exec(this.options.path)
+  compiler.plugin("this-compilation", function(compilation){
+
+      shell.exec('chmod +x ' +__dirname + "/version.sh");
+
+      var result = shell.exec(__dirname + "/version.sh")
         .output
         .replace(/\s/g, '');
 
-      console.log(result);
-    }
-    callback(null, null);
-  });
+      this.options.buildVersion = result;
+  })
 };
 
 module.exports = Plugin;
---
+++ version.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#   version.sh
+#   Generate version string for a git directory
+#
+# Usage:
+#
+#   Generate version string for landing app
+#   $ ./version.sh ../landing
+
+# Error out on first non-true command
+set -e
+
+# Use first argument as build task
+GIT_DIR="$1"
+
+# Get working directory
+PATH_TO_SOURCE_SCRIPT="${BASH_SOURCE[0]}"
+PATH_TO_SOURCE_DIR="$( cd "$( dirname "${SOURCE}" )" && pwd )"
+PATH_TO_ROOT_DIR="$(git rev-parse --show-toplevel)"
+
+# Build version string
+BUILD_TIMESTAMP="$(date +"%Y-%m-%d-%r-%Z")"
+BUILD_TIMESTAMP="${BUILD_TIMESTAMP/ /-}"
+GIT_BRANCH="$(git symbolic-ref --short -q HEAD || echo 'HEAD')"
+GIT_TAG="$(git describe --always)"
+GIT_USER="$(git show --format='%aN' HEAD)"
+
+
+BUILD_VERSION="[${BUILD_TIMESTAMP}]-[${GIT_BRANCH}]-[${GIT_TAG}]-[${GIT_USER}]"
+
+echo $BUILD_VERSION
